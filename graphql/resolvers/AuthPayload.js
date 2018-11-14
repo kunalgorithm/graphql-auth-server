03b7a636635d265386b8cca2c91cb3e898d388a2
Kunal <kshah239@gmail.com>
2018-11-14 16:07:33 +0800

    authentication payloads

--- graphql/resolvers/AuthPayload.js
+++ graphql/resolvers/AuthPayload.js
@@ -3,6 +3,7 @@ import User from "../../models/User";
 export default {
   Query: {
     user: (root, args) => {
+      console.log("Authpayload resolved called for user.");
       return new Promise((resolve, reject) => {
         User.findOne(args).exec((err, res) => {
           err ? reject(err) : resolve(res);
